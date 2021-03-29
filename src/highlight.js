8932373c40f333c8b21d3d60a275519305030fb0
David R. Myers <davidrmyersii@gmail.com>
2021-03-28 20:57:50 -0400

    Improve default theme and style configuration

--- src/highlight.js
+++ src/highlight.js
@@ -1,107 +1,104 @@
 import { HighlightStyle, tags } from '@codemirror/highlight'
 
 const SyntaxHighlighting = HighlightStyle.define([
+  // ordered by lowest to highest precedence
   {
     tag: tags.atom,
     color: 'var(--hybrid-mde-atom, #d19a66)',
   },
   {
-    tag: tags.bool,
-    class: "cmt-bool",
-  },
-  {
-    tag: tags.comment,
-    color: 'var(--hybrid-mde-comment, #5c6370)',
-    fontStyle: 'italic',
-  },
-  {
-    tag: tags.definition(tags.variableName),
-    color: 'var(--hybrid-mde-definition, #e5c07b)',
-  },
-  {
-    tag: tags.deleted,
-    class: "cmt-deleted",
-  },
-  {
-    tag: tags.emphasis,
-    fontStyle: 'italic',
-  },
-  // meta has to be set before headings for precedence
-  {
     tag: tags.meta,
     color: 'var(--hybrid-mde-meta, #abb2bf)',
   },
   {
-    tag: tags.heading1,
+    tag: tags.processingInstruction,
+    color: 'var(--hybrid-mde-processingInstruction, #abb2bf)',
+  },
+  // comment group
+  {
+    tag: tags.comment,
+    color: 'var(--hybrid-mde-comment, #abb2bf)',
+    fontStyle: 'italic',
+  },
+  // name group
+  {
+    tag: tags.name,
+    color: 'var(--hybrid-mde-name, #d19a66)',
+  },
+  {
+    tag: tags.labelName,
+    color: 'var(--hybrid-mde-labelName, var(--hybrid-mde-name, #abb2bf))',
+  },
+  {
+    tag: tags.propertyName,
+    color: 'var(--hybrid-mde-propertyName, var(--hybrid-mde-name, #96c0d8))',
+  },
+  {
+    tag: tags.definition(tags.propertyName),
+    color: 'var(--hybrid-mde-propertyName-definition, var(--hybrid-mde-propertyName, var(--hybrid-mde-name, #e06c75)))',
+  },
+  {
+    tag: tags.variableName,
+    color: 'var(--hybrid-mde-variableName, var(--hybrid-mde-name, #e06c75))',
+  },
+  {
+    tag: tags.definition(tags.variableName),
+    color: 'var(--hybrid-mde-variableName-definition, var(--hybrid-mde-variableName, var(--hybrid-mde-name, #e5c07b)))',
+  },
+  {
+    tag: tags.local(tags.variableName),
+    color: 'var(--hybrid-mde-variableName-local, var(--hybrid-mde-variableName, var(--hybrid-mde-name, #d19a66)))',
+  },
+  {
+    tag: tags.special(tags.variableName),
+    color: 'var(--hybrid-mde-variableName-special, var(--hybrid-mde-variableName, var(--hybrid-mde-name, inherit)))',
+  },
+  // headings
+  {
+    tag: tags.heading,
     color: 'var(--hybrid-mde-heading, #e06c75)',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading1,
+    color: 'var(--hybrid-mde-heading1, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.6em',
     fontWeight: 'bold',
   },
   {
     tag: tags.heading2,
-    color: 'var(--hybrid-mde-heading, #e06c75)',
+    color: 'var(--hybrid-mde-heading2, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.5em',
     fontWeight: 'bold',
   },
   {
     tag: tags.heading3,
-    color: 'var(--hybrid-mde-heading, #e06c75)',
+    color: 'var(--hybrid-mde-heading3, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.4em',
     fontWeight: 'bold',
   },
   {
     tag: tags.heading4,
-    color: 'var(--hybrid-mde-heading, #e06c75)',
+    color: 'var(--hybrid-mde-heading4, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.3em',
     fontWeight: 'bold',
   },
   {
     tag: tags.heading5,
-    color: 'var(--hybrid-mde-heading, #e06c75)',
+    color: 'var(--hybrid-mde-heading5, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.2em',
     fontWeight: 'bold',
   },
   {
     tag: tags.heading6,
-    color: 'var(--hybrid-mde-heading, #e06c75)',
+    color: 'var(--hybrid-mde-heading6, var(--hybrid-mde-heading, #e06c75))',
     fontSize: '1.1em',
     fontWeight: 'bold',
   },
-  {
-    tag: tags.inserted,
-    class: "cmt-inserted",
-  },
-  {
-    tag: tags.invalid,
-    class: "cmt-invalid",
-  },
+  // contextual tag types
   {
     tag: tags.keyword,
     color: 'var(--hybrid-mde-keyword, #c678dd)',
-  },
-  {
-    tag: tags.labelName,
-    class: "cmt-labelName",
-  },
-  {
-    tag: tags.link,
-    color: 'var(--hybrid-mde-link, #96c0d8)',
-  },
-  {
-    tag: tags.literal,
-    class: "cmt-literal",
-  },
-  {
-    tag: tags.local(tags.variableName),
-    class: "cmt-variableName cmt-local",
-  },
-  {
-    tag: tags.macroName,
-    class: "cmt-macroName",
-  },
-  {
-    tag: tags.namespace,
-    class: "cmt-namespace",
   },
   {
     tag: tags.number,
@@ -112,40 +109,36 @@ const SyntaxHighlighting = HighlightStyle.define([
     color: 'var(--hybrid-mde-operator, #96c0d8)',
   },
   {
-    tag: tags.propertyName,
-    color: 'var(--hybrid-mde-propertyName, #d19a66)',
-  },
-  {
     tag: tags.punctuation,
-    color: 'var(--hybrid-mde-punctuation, #abb2bf)',
+    color: 'var(--hybrid-mde-punctuation, #36454f)',
   },
   {
-    tag: tags.special(tags.variableName),
-    class: "cmt-variableName2",
-  },
-  {
-    tag: tags.string,
-    color: 'var(--hybrid-mde-string, #98c379)',
-  },
-  {
-    tag: tags.strong,
-    fontWeight: 'bold',
-  },
-  {
-    tag: tags.typeName,
-    class: "cmt-typeName",
+    tag: tags.link,
+    color: 'var(--hybrid-mde-link, #96c0d8)',
   },
   {
     tag: tags.url,
     color: 'var(--hybrid-mde-url, #96c0d8)',
   },
+  // string group
   {
-    tag: tags.variableName,
-    color: 'var(--hybrid-mde-variableName, #e06c75)',
+    tag: tags.string,
+    color: 'var(--hybrid-mde-string, #98c379)',
   },
   {
-    tag: [tags.regexp, tags.escape, tags.special(tags.string)],
-    class: "cmt-string2",
+    tag: tags.special(tags.string),
+    color: 'var(--hybrid-mde-string-special, var(--hybrid-mde-string, inherit))',
+  },
+  // emphasis types
+  {
+    tag: tags.emphasis,
+    color: 'var(--hybrid-mde-emphasis, inherit)',
+    fontStyle: 'italic',
+  },
+  {
+    tag: tags.strong,
+    color: 'var(--hybrid-mde-strong, inherit)',
+    fontWeight: 'bold',
   },
 ])
 
