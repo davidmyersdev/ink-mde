0a6c5380935322c4ca9fb4ec1f91d0342432364d
David R. Myers <davidrmyersii@gmail.com>
2021-09-26 16:48:11 -0400

    Move to Vite

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Ink',
+      fileName: (format) => `ink.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      output: [{
+        esModule: true,
+        exports: 'named',
+        format: 'es',
+      }, {
+        format: 'umd',
+        inlineDynamicImports: true,
+        interop: 'esModule',
+        exports: 'named',
+      }]
+    }
+  },
+})
