b1203e6bda9669dc31da0582a08a907022acee75
David R. Myers <davidrmyersii@gmail.com>
2022-02-16 22:55:31 -0500

    Refactor pretty much everything
    This refactor moves a lot of things around and definitely breaks the previous API. The types have been reorganized and rewritten. The configuration has been organized and mostly centralized. The previously exposed CodeMirror types have been mostly encapsulated. Going forward, all public APIs for Ink will completely encapsulate any underlying libraries and be adapted internally. At the moment, it's difficult to allow Ink to accept CodeMirror extensions directly due to the instanceof checks in the CM6 codebase. If this issue is resolved, we might want to expose an explicit way to supply CM6 extensions. That said, exposing such an API would create a heavy coupling on CodeMirror.

---
+++ types/legacy.ts
@@ -0,0 +1,13 @@
+import type { EditorSelection, Extension } from '@codemirror/state'
+import type Ink from '/types/ink'
+
+export interface InkLegacyOptions {
+  appearance: Ink.Values.Appearance
+  attribution: boolean
+  doc: string
+  extensions: Extension[]
+  images: boolean
+  spellcheck: boolean
+  onChange: (doc: string) => void
+  selection?: EditorSelection
+}
---
+++ types/ui.ts
@@ -0,0 +1,9 @@
+export namespace InkUi {
+  export namespace App {
+    export interface Props {
+      dom?: HTMLElement
+    }
+  }
+}
+
+export default InkUi
