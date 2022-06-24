741437c9d0ea700a0ca9997bdfc35e28c7d8d5ad
David R. Myers <davidrmyersii@gmail.com>
2022-06-24 11:52:34 -0400

    Merge the Vue component package into this repo

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -1,45 +1,46 @@
-/// <reference types="vitest" />
-
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, './src/index.ts'),
+      entry: resolve(__dirname, 'src/components/Ink.vue'),
+      name: 'Ink',
       fileName: (format) => {
         if (format === 'es') { return 'ink.js' }
         if (format === 'umd') { return 'ink.cjs' }
 
         return `ink.${format}.cjs`
       },
-      name: 'ink',
     },
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['@writewithocto/ink', 'vue'],
       output: [
         {
           esModule: true,
           exports: 'named',
           format: 'es',
+          globals: {
+            vue: 'Vue'
+          },
         },
         {
           exports: 'named',
           format: 'umd',
           inlineDynamicImports: true,
           interop: 'esModule',
+          globals: {
+            vue: 'Vue'
+          },
         },
-      ],
-    },
+      ]
+    }
   },
   plugins: [
-    svelte({ emitCss: false }),
+    vue(),
   ],
-  resolve: {
-    alias: {
-      '/types': resolve(__dirname, './types'),
-      '@writewithocto/ink': resolve(__dirname, './src/index'),
-    },
-  },
 })
