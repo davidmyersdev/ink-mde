62a1d62edb6861d17bd681caedf3bd0e51be70bd
David R. Myers <davidrmyersii@gmail.com>
2022-08-05 13:38:32 -0400

    Use a circular-ref-safe merge strategy

--- src/utils/deepmerge.ts
+++ src/utils/deepmerge.ts
@@ -1,14 +1,53 @@
-import { deepmergeCustom } from 'deepmerge-ts'
+const types = {
+  array: '[object Array]',
+  object: '[object Object]',
+  string: '[object String]',
+  undefined: '[object Undefined]',
+  window: '[object Window]',
+}
 
-export const deepmergePartial = deepmergeCustom({
-  mergeOthers: (values, utils, _meta) => {
-    if (typeof values === 'undefined')
-      return utils.actions.skip
+const getType = (object: any) => {
+  const type = Object.prototype.toString.call(object)
 
-    return utils.actions.defaultMerge
-  },
-})
+  if (type === types.object) {
+    return `[object ${object.constructor.name}]`
+  }
+}
 
-export const override = <T>(a: T, ...rest: unknown[]): T => {
-  return deepmergePartial(a, ...rest) as unknown as T
+const is = (object: any, type: string) => {
+  return getType(object) === type
+}
+
+const deepAssign = (target: any, source: any) => {
+  const seen = new WeakMap()
+
+  const assign = (target: any, source: any) => {
+    if (seen.get(target)) return target
+    if (is(target, types.object)) seen.set(target, true)
+    if (is(source, types.undefined)) return target
+
+    if (is(target, types.array) && is(source, types.array)) {
+      return [...source]
+    }
+
+    if (is(target, types.object) && is(source, types.object)) {
+      return Object.keys(target).reduce((replacement: Record<PropertyKey, unknown>, key: PropertyKey) => {
+        if (Object.hasOwn(source, key)) {
+          replacement[key] = assign(target[key], source[key])
+        } else {
+          replacement[key] = target[key]
+        }
+
+        return replacement
+      }, {})
+    }
+
+    return source
+  }
+
+  return assign(target, source)
+}
+
+export const override = <T>(a: T, b: unknown): T => {
+  return deepAssign(a, b) as unknown as T
 }
