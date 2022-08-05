f05596a3accc1163572432b0a4270c9c422f5661
David R. Myers <davidrmyersii@gmail.com>
2022-08-05 13:42:27 -0400

    Fix broken reactivity

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,11 +1,11 @@
 import { createContext, useContext } from 'solid-js'
-import { blankState } from '../store'
-import { Root } from './root'
 import type { Component, JSX } from 'solid-js'
 import type InkInternal from '/types/internal'
+import { blankState } from '../store'
+import { Root } from './root'
 
 const defaultState = blankState()
-const AppContext = createContext<InkInternal.Store>([defaultState, () => defaultState])
+const AppContext = createContext<InkInternal.Store>([() => defaultState, value => (typeof value === 'function' ? value(defaultState) : value)])
 const AppProvider: Component<{ children: JSX.Element, store: InkInternal.Store }> = (props) => {
   return (
     <AppContext.Provider value={props.store}>
@@ -21,7 +21,7 @@ export const useStore = () => {
 export const App: Component<{ store: InkInternal.Store }> = (props) => {
   return (
     <AppProvider store={props.store}>
-      <Root />
+      <Root store={props.store} />
     </AppProvider>
   )
 }
