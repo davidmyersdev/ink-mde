18424e32849b7ed06009c9ae4fcb6f0ee861cd02
David R. Myers <davidrmyersii@gmail.com>
2022-08-30 16:36:37 -0400

    Automatically hydrate or render based on SSR usage

--- vite.config.ts
+++ vite.config.ts
@@ -6,59 +6,62 @@ import solidjs from 'vite-plugin-solid'
 import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, './src/index.tsx'),
-      fileName: (format) => {
-        if (format === 'cjs')
-          return 'ink.cjs'
-        if (format === 'es')
-          return 'ink.js'
-
-        return `ink.${format}.js`
+export default defineConfig(({ ssrBuild }) => {
+  return {
+    build: {
+      emptyOutDir: !ssrBuild,
+      lib: {
+        entry: './src/index.tsx',
+        fileName: 'client',
+        formats: [],
       },
-      formats: ['es', 'cjs'],
+      rollupOptions: {
+        output: [
+          {
+            esModule: true,
+            exports: 'named',
+            format: 'es',
+          },
+          {
+            exports: 'named',
+            format: 'cjs',
+            inlineDynamicImports: true,
+            interop: 'esModule',
+          },
+        ],
+      },
+      sourcemap: true,
+      target: 'esnext',
     },
-    rollupOptions: {
-      output: [
-        {
-          esModule: true,
-          exports: 'named',
-          format: 'es',
+    plugins: [
+      externalizeDeps(),
+      solidjs({
+        solid: {
+          generate: ssrBuild ? 'ssr' : 'dom',
+          hydratable: true,
         },
-        {
-          exports: 'named',
-          format: 'cjs',
-          inlineDynamicImports: true,
-          interop: 'esModule',
-        },
-      ],
-    },
-    target: 'esnext',
-  },
-  plugins: [
-    externalizeDeps(),
-    solidjs(),
-  ],
-  resolve: {
-    alias: {
-      '/test': resolve(__dirname, './test'),
-      '/types': resolve(__dirname, './types'),
-      'ink-mde': resolve(__dirname, './src/index'),
-    },
-    conditions: [
-      'browser',
+      }),
     ],
-  },
-  test: {
-    deps: {
-      inline: [
-        'solid-js',
+    resolve: {
+      alias: {
+        '/': resolve(__dirname, './'),
+        'ink-mde': resolve(__dirname, './src/index'),
+      },
+      conditions: [
+        'browser',
+        'node',
+        'solid',
       ],
     },
-    transformMode: {
-      web: [/\.tsx?$/],
+    test: {
+      deps: {
+        inline: [
+          'solid-js',
+        ],
+      },
+      transformMode: {
+        web: [/\.tsx?$/],
+      },
     },
-  },
+  }
 })
