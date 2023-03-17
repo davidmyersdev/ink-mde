b81985596b37797d150206d93dd5ef25485f693e
David R. Myers <davidrmyersii@gmail.com>
2023-03-17 09:18:25 -0400

    Add a new function to wrap textareas

--- examples/minimal.ts
+++ examples/minimal.ts
@@ -1,4 +1,4 @@
-import ink from 'ink-mde'
+import { ink } from 'ink-mde'
 
 // The only requirement is an HTML element.
 ink(document.getElementById('editor')!)
--- src/index.tsx
+++ src/index.tsx
@@ -59,4 +59,21 @@ export const solidPrepareForHydration = () => {
   let e,t;e=window._$HY||(window._$HY={events:[],completed:new WeakSet,r:{}}),t=e=>e&&e.hasAttribute&&(e.hasAttribute("data-hk")?e:t(e.host&&e.host instanceof Node?e.host:e.parentNode)),['click', 'input'].forEach((o=>document.addEventListener(o,(o=>{let s=o.composedPath&&o.composedPath()[0]||o.target,a=t(s);a&&!e.completed.has(a)&&e.events.push([a,o])})))),e.init=(t,o)=>{e.r[t]=[new Promise(((e,t)=>o=e)),o]},e.set=(t,o,s)=>{(s=e.r[t])&&s[1](o),e.r[t]=[o]},e.unset=t=>{delete e.r[t]},e.load=(t,o)=>{if(o=e.r[t])return o[0]}
 }
 
+export const wrap = (textarea: HTMLTextAreaElement, options: Ink.Options = {}) => {
+  const replacement = <div class='ink-mde-textarea' /> as HTMLDivElement
+
+  textarea.after(replacement)
+  textarea.style.display = 'none'
+
+  const instance = render(textarea, options)
+
+  if (textarea.form) {
+    textarea.form.addEventListener('submit', () => {
+      textarea.value = instance.getDoc()
+    })
+  }
+
+  return instance
+}
+
 export default ink
