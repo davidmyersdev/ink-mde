a32bdb8a2ae335194c9ec4b16d749bee6a40d52e
David R. Myers <davidrmyersii@gmail.com>
2023-03-26 17:50:23 -0400

    Extract utils to help build plugins

--- examples/plugins.tsx
+++ examples/plugins.tsx
@@ -1,152 +1,42 @@
-import { syntaxTree } from '@codemirror/language'
-import { type EditorState, type Extension, type Range, type RangeCursor, RangeSet, StateField } from '@codemirror/state'
-import { EditorView } from '@codemirror/view'
 import { ink } from 'ink-mde'
-import { type Signal, createRoot, createSignal } from 'solid-js'
-import { buildWidgetDecoration } from '/lib/codemirror-kit'
-
-const hasOverlap = (x1: number, x2: number, y1: number, y2: number) => {
-  return Math.max(x1, y1) <= Math.min(x2, y2)
-}
-
-const preview = (text: string, from: number, to: number, [counters, setCounters]: Signal<Record<string, number>>) => {
-  setCounters({ ...counters(), buildDecoration: counters().buildDecoration + 1 })
-
-  return buildWidgetDecoration({
-    block: true,
-    side: -1,
-    widget: {
-      id: text,
-      length: to - from,
-      toDOM: () => {
-        setCounters({ ...counters(), toDOM: counters().toDOM + 1 })
-
-        return (
-          <div>
-            <div>Decoration function calls: {counters().buildDecoration}</div>
-            <div>Decoration DOM updates: {counters().toDOM}</div>
-            <div>DOM updated at: {Date.now()}</div>
-            <span>{text}</span>
-          </div>
-        ) as HTMLElement
-      },
-    },
-  })
-}
-
-export const codeBlockDecorator = (): Extension => {
-  return createRoot(() => {
-    const [counters, setCounters] = createSignal({ buildDecoration: 0, toDOM: 0 })
-
-    const decorate = (state: EditorState, from?: number, to?: number) => {
-      const widgets: Range<ReturnType<typeof preview>>[] = []
-
-      syntaxTree(state).iterate({
-        enter: ({ type, from, to }) => {
-          // Only look at fenced code for right now.
-          if (type.name === 'FencedCode') {
-            // Grab all text between the opening and closing lines.
-            const text = state.sliceDoc(from, to).split('\n').slice(1, -1).join('\n')
-
-            if (text) {
-              widgets.push(preview(text, from, to, [counters, setCounters]).range(from))
-            }
-          }
-        },
-        from,
-        to,
-      })
-
-      return widgets
-    }
-
-    const stateField = StateField.define<RangeSet<ReturnType<typeof preview>>>({
-      create(state) {
-        return RangeSet.of(decorate(state))
-      },
-      update(rangeSet, transaction) {
-        // Reconfiguration and state effects will reprocess the entire state to ensure nothing is missed.
-        if (transaction.reconfigured || transaction.effects.length > 0) {
-          return RangeSet.of(decorate(transaction.state))
-        }
-
-        const updatedRangeSet = rangeSet.map(transaction.changes)
-
-        // Doc changes should only process the affected ranges.
-        if (transaction.docChanged) {
-          const decorations = [] as ReturnType<typeof decorate>
-          const cursor = updatedRangeSet.iter()
-          const cursors = [] as RangeCursor<ReturnType<typeof preview>>[]
-          const cursorsToSkip = [] as RangeCursor<ReturnType<typeof preview>>[]
-
-          while (cursor.value) {
-            cursors.push({ ...cursor })
-            cursor.next()
-          }
-
-          transaction.changes.iterChangedRanges((_beforeFrom, _beforeTo, afterFrom, afterTo) => {
-            cursors.forEach((cursor) => {
-              const cursorTo = cursor.from + (cursor.value?.widget.length || 0)
-
-              if (hasOverlap(cursor.from, cursorTo, afterFrom, afterTo)) {
-                cursorsToSkip.push(cursor)
-              }
-            })
-
-            decorations.push(...decorate(transaction.state, afterFrom, afterTo))
-          })
-
-          const cursorDecos = cursors.filter(cursor => !cursorsToSkip.includes(cursor)).flatMap((cursor) => {
-            const range = cursor.value?.range(cursor.from)
-
-            if (!range) return []
-
-            return [range]
-          })
-
-          decorations.push(...cursorDecos)
-
-          // I think I need the entire arrays of ranges and changes ahead of time.
-          // If a given range has *no* related changes, then it can be kept in the list of decorations.
-          // If a given change has no relevant ranges, then it can be processed to create potential new decorations.
-          //
-          // Maybe the logic is this:
-          // If a given change is entirely encapsulated within a decoration range,
-          // then that decoration alone can be updated specifically for that change.
-          // If a given change is partially encapsulated within a decoration range,
-          // then that decoration should be dropped, and the change should be used to calculate a new decoration set.
-          // If a given change is not related to a decoration range,
-          // then that change should be used to create new decorations without affecting existing decorations.
-
-          const allDecorations = decorations.sort((left, right) => {
-            return left.from - right.from
-          })
-
-          // This reprocesses the entire state.
-          return RangeSet.of(allDecorations)
-        }
-
-        // No need to redecorate. Instead, just map the decorations through the transaction changes.
-        return updatedRangeSet
-      },
-      provide(field) {
-        // Provide the extension to the editor.
-        return EditorView.decorations.from(field)
-      },
-    })
-
-    return [
-      stateField,
-    ]
-  })
-}
+import { buildBlockWidgetDecoration, nodeDecorator } from '/lib/codemirror-kit'
 
 ink(document.querySelector('#app')!, {
   doc: '# Start with some text\n\n```\nhi\n```\n\n```\nhello\n```',
   plugins: [
     {
       type: 'default',
-      value: codeBlockDecorator(),
+      value: nodeDecorator({
+        nodes: ['FencedCode'],
+        onMatch: (state, node) => {
+          const text = state.sliceDoc(node.from, node.to).split('\n').slice(1, -1).join('\n')
+
+          if (text) {
+            return buildBlockWidgetDecoration({
+              widget: {
+                // You can see the results of optimization when there is no id specified. Because CodeMirror has no way of knowing
+                // whether the decoration matches, it usually has to rebuild the DOM on each change. With the optimization setting,
+                // the DOM only has to be rebuilt if the changes overlap with the existing decoration.
+                //
+                // With optimization enabled, the updated timestamp only updates when something inside that block changes.
+                // With optimization disabled, the updated timestamp updates on *any* doc change.
+                //
+                // id: text,
+                toDOM: () => {
+                  return (
+                    <div>
+                      <div>DOM updated at: {Date.now()}</div>
+                      <span>{text}</span>
+                    </div>
+                  ) as HTMLElement
+                },
+              },
+            })
+          }
+        },
+        // When set to true, only the nodes that overlap changed ranges will be reprocessed.
+        optimize: true,
+      }),
     },
   ],
 })
