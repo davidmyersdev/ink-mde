e24e12cb9f1f5c0d617d110b3efa6b030c5936fb
David R. Myers <davidrmyersii@gmail.com>
2023-04-02 12:23:11 -0400

    Update my handle

--- src/ui/components/details/index.tsx
+++ src/ui/components/details/index.tsx
@@ -20,7 +20,7 @@ export const Details: Component<{ store: InkInternal.Store }> = () => {
           </Show>
           <Show when={state().options.interface.attribution}>
             <div class='ink-mde-attribution'>
-              <span>&nbsp;powered by <a class='ink-mde-attribution-link' href='https://github.com/voracious/ink-mde' rel='noopener noreferrer' target='_blank'>ink-mde</a></span>
+              <span>&nbsp;powered by <a class='ink-mde-attribution-link' href='https://github.com/davidmyersdev/ink-mde' rel='noopener noreferrer' target='_blank'>ink-mde</a></span>
             </div>
           </Show>
         </div>
