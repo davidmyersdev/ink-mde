6b1696e71cecc64dc9021901a6145ae5c9ce661f
David R. Myers <davidrmyersii@gmail.com>
2023-04-19 09:35:07 -0400

    More Plugin API updates

--- plugins/katex/index.ts
+++ plugins/katex/index.ts
@@ -1,6 +1,6 @@
 import { HighlightStyle, syntaxHighlighting } from '@codemirror/language'
 import { EditorView } from '@codemirror/view'
-import { definePlugin, inkPlugin, pluginTypes } from 'ink-mde'
+import { plugin, pluginTypes } from 'ink-mde'
 import { useModule } from '/src/modules'
 import { buildBlockWidgetDecoration, buildLineDecoration, buildWidget, nodeDecorator } from '/lib/codemirror-kit'
 import { grammar, mathInline, mathInlineMark, mathInlineMarkClose, mathInlineMarkOpen } from './grammar'
@@ -12,105 +12,117 @@ const render = (text: string, element: HTMLElement) => {
 }
 
 export const katex = () => {
-  return definePlugin([
-    inkPlugin(pluginTypes.grammar, () => grammar),
-    inkPlugin(pluginTypes.default, () => {
-      return nodeDecorator({
-        nodes: ['MathBlock', 'MathBlockMarkClose', 'MathBlockMarkOpen'],
-        onMatch: (_state, node) => {
-          // Either: only match the top node (inline) and iterate over the children, or: match both and keep track of opening/closing tokens.
-          // Another option: Update the grammar to create separate open/close nodes, and maybe make them children/parents of the more generic "mark" node?
-          // Look at the CodeMirror examples to see if this has been done for other syntax nodes, and also check out the extensions to see how highlighting of matching brackets, etc has been done.
-          const classes = ['ink-mde-line-math-block']
-
-          if (node.name === 'MathBlockMarkOpen') classes.push('ink-mde-line-math-block-open')
-          if (node.name === 'MathBlockMarkClose') classes.push('ink-mde-line-math-block-close')
-
-          return buildLineDecoration({
-            attributes: {
-              class: classes.join(' '),
-            },
-          })
-        },
-        // When set to true, only the nodes that overlap changed ranges will be reprocessed.
-        optimize: false,
-      })
+  return [
+    plugin({
+      key: 'katex',
+      type: pluginTypes.grammar,
+      value: async () => grammar,
     }),
-    inkPlugin(pluginTypes.default, async () => {
-      return nodeDecorator({
-        nodes: ['MathBlock'],
-        onMatch: (state, node) => {
-          const text = state.sliceDoc(node.from, node.to).split('\n').slice(1, -1).join('\n')
+    plugin({
+      key: 'katex',
+      value: async () => {
+        return nodeDecorator({
+          nodes: ['MathBlock', 'MathBlockMarkClose', 'MathBlockMarkOpen'],
+          onMatch: (_state, node) => {
+            const classes = ['ink-mde-line-math-block']
 
-          if (text) {
-            return buildBlockWidgetDecoration({
-              widget: buildWidget({
-                id: text,
-                toDOM: () => {
-                  const container = document.createElement('div')
-                  const block = document.createElement('div')
+            if (node.name === 'MathBlockMarkOpen') classes.push('ink-mde-line-math-block-open')
+            if (node.name === 'MathBlockMarkClose') classes.push('ink-mde-line-math-block-close')
 
-                  container.className = 'ink-mde-block-widget-container'
-                  block.className = 'ink-mde-block-widget ink-mde-katex-target'
-                  container.appendChild(block)
-
-                  render(text, block)
-
-                  return container
-                },
-                updateDOM: (dom) => {
-                  const katexTarget = dom.querySelector<HTMLElement>('.ink-mde-katex-target')
-
-                  if (katexTarget) {
-                    render(text, katexTarget)
-
-                    return true
-                  }
-
-                  return false
-                },
-              }),
+            return buildLineDecoration({
+              attributes: {
+                class: classes.join(' '),
+              },
             })
-          }
-        },
-        optimize: false,
-      })
-    }, 'katex'),
-    inkPlugin(pluginTypes.default, () => {
-      return syntaxHighlighting(
-        HighlightStyle.define([
-          {
-            tag: [mathInline.tag, mathInlineMark.tag],
-            backgroundColor: 'var(--ink-internal-block-background-color)',
           },
-          {
-            tag: [mathInlineMarkClose.tag],
-            backgroundColor: 'var(--ink-internal-block-background-color)',
-            borderRadius: '0 var(--ink-internal-border-radius) var(--ink-internal-border-radius) 0',
-            paddingRight: 'var(--ink-internal-inline-padding)',
-          },
-          {
-            tag: [mathInlineMarkOpen.tag],
-            backgroundColor: 'var(--ink-internal-block-background-color)',
-            borderRadius: 'var(--ink-internal-border-radius) 0 0 var(--ink-internal-border-radius)',
-            paddingLeft: 'var(--ink-internal-inline-padding)',
-          },
-        ]),
-      )
+          optimize: false,
+        })
+      },
     }),
-    inkPlugin(pluginTypes.default, () => {
-      return EditorView.theme({
-        '.ink-mde-line-math-block': {
-          backgroundColor: 'var(--ink-internal-block-background-color)',
-          padding: '0 var(--ink-internal-block-padding) !important',
-        },
-        '.ink-mde-line-math-block-open': {
-          borderRadius: 'var(--ink-internal-border-radius) var(--ink-internal-border-radius) 0 0',
-        },
-        '.ink-mde-line-math-block-close': {
-          borderRadius: '0 0 var(--ink-internal-border-radius) var(--ink-internal-border-radius)',
-        },
-      })
+    plugin({
+      key: 'katex',
+      value: async () => {
+        return nodeDecorator({
+          nodes: ['MathBlock'],
+          onMatch: (state, node) => {
+            const text = state.sliceDoc(node.from, node.to).split('\n').slice(1, -1).join('\n')
+
+            if (text) {
+              return buildBlockWidgetDecoration({
+                widget: buildWidget({
+                  id: text,
+                  toDOM: () => {
+                    const container = document.createElement('div')
+                    const block = document.createElement('div')
+
+                    container.className = 'ink-mde-block-widget-container'
+                    block.className = 'ink-mde-block-widget ink-mde-katex-target'
+                    container.appendChild(block)
+
+                    render(text, block)
+
+                    return container
+                  },
+                  updateDOM: (dom) => {
+                    const katexTarget = dom.querySelector<HTMLElement>('.ink-mde-katex-target')
+
+                    if (katexTarget) {
+                      render(text, katexTarget)
+
+                      return true
+                    }
+
+                    return false
+                  },
+                }),
+              })
+            }
+          },
+          optimize: false,
+        })
+      },
     }),
-  ])
+    plugin({
+      key: 'katex',
+      value: async () => {
+        return syntaxHighlighting(
+          HighlightStyle.define([
+            {
+              tag: [mathInline.tag, mathInlineMark.tag],
+              backgroundColor: 'var(--ink-internal-block-background-color)',
+            },
+            {
+              tag: [mathInlineMarkClose.tag],
+              backgroundColor: 'var(--ink-internal-block-background-color)',
+              borderRadius: '0 var(--ink-internal-border-radius) var(--ink-internal-border-radius) 0',
+              paddingRight: 'var(--ink-internal-inline-padding)',
+            },
+            {
+              tag: [mathInlineMarkOpen.tag],
+              backgroundColor: 'var(--ink-internal-block-background-color)',
+              borderRadius: 'var(--ink-internal-border-radius) 0 0 var(--ink-internal-border-radius)',
+              paddingLeft: 'var(--ink-internal-inline-padding)',
+            },
+          ]),
+        )
+      },
+    }),
+    plugin({
+      key: 'katex',
+      value: async () => {
+        return EditorView.theme({
+          '.ink-mde-line-math-block': {
+            backgroundColor: 'var(--ink-internal-block-background-color)',
+            padding: '0 var(--ink-internal-block-padding) !important',
+          },
+          '.ink-mde-line-math-block-open': {
+            borderRadius: 'var(--ink-internal-border-radius) var(--ink-internal-border-radius) 0 0',
+          },
+          '.ink-mde-line-math-block-close': {
+            borderRadius: '0 0 var(--ink-internal-border-radius) var(--ink-internal-border-radius)',
+          },
+        })
+      },
+    }),
+  ]
 }
