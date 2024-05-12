5c8c045e0c6033db484758bdf93a2fac8b0b73c1
David R. Myers <davidrmyersii@gmail.com>
2024-05-11 21:37:40 -0400

    Remove solid-js from search extension

--- src/editor/extensions/search.tsx
+++ src/editor/extensions/search.tsx
@@ -1,53 +1,65 @@
-import { SearchQuery, findNext, findPrevious, getSearchQuery, search as searchExtension, searchKeymap, setSearchQuery } from '@codemirror/search'
+import {
+  SearchQuery,
+  findNext,
+  findPrevious,
+  getSearchQuery,
+  search as searchExtension,
+  searchKeymap,
+  setSearchQuery,
+} from '@codemirror/search'
 import { keymap, runScopeHandlers } from '@codemirror/view'
-import { createRoot, createSignal } from 'solid-js'
 
 export const search = () => {
   return [
     searchExtension({
       top: true,
       createPanel: (view) => {
-        return createRoot((dispose) => {
-          const [query, setQuery] = createSignal(getSearchQuery(view.state))
-          let el: HTMLInputElement | undefined
+        let query = getSearchQuery(view.state)
 
-          const handleKeyDown = (event: KeyboardEvent) => {
-            if (runScopeHandlers(view, event, 'search-panel')) return event.preventDefault()
+        const wrapper = document.createElement('div')
+        const input = document.createElement('input')
 
-            if (event.code === 'Enter') {
-              event.preventDefault()
+        wrapper.setAttribute('class', 'ink-mde-search-panel')
+        input.setAttribute('attr:main-field', 'true')
+        input.setAttribute('class', 'ink-mde-search-input')
+        input.setAttribute('type', 'text')
+        input.setAttribute('value', query.search)
 
-              if (event.shiftKey) {
-                findPrevious(view)
-              } else {
-                findNext(view)
-              }
+        wrapper.appendChild(input)
+
+        const handleKeyDown = (event: KeyboardEvent) => {
+          if (runScopeHandlers(view, event, 'search-panel')) return event.preventDefault()
+
+          if (event.code === 'Enter') {
+            event.preventDefault()
+
+            if (event.shiftKey) {
+              findPrevious(view)
+            } else {
+              findNext(view)
             }
           }
+        }
 
-          const updateSearch = (event: InputEvent) => {
-            // @ts-expect-error "value" is not a recognized property of EventTarget.
-            const { value } = event.target
+        const updateSearch = (event: Event) => {
+          // @ts-expect-error "value" is not a recognized property of EventTarget.
+          const { value } = event.target
 
-            setQuery(new SearchQuery({ search: value }))
-            view.dispatch({ effects: setSearchQuery.of(query()) })
-          }
+          query = new SearchQuery({ search: value })
 
-          return {
-            destroy: () => {
-              dispose()
-            },
-            dom: (
-              <div class='ink-mde-search-panel' onKeyDown={handleKeyDown}>
-                <input attr:main-field='true' class='ink-mde-search-input' onInput={updateSearch} onKeyDown={handleKeyDown} ref={el} type="text" value={query().search} />
-              </div>
-            ) as HTMLElement,
-            mount: () => {
-              el?.focus()
-            },
-            top: true,
-          }
-        })
+          view.dispatch({ effects: setSearchQuery.of(query) })
+        }
+
+        input.addEventListener('input', updateSearch)
+        input.addEventListener('keydown', handleKeyDown)
+
+        return {
+          dom: wrapper,
+          mount: () => {
+            input.focus()
+          },
+          top: true,
+        }
       },
     }),
     keymap.of(searchKeymap),
