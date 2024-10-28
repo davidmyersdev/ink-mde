17da2a490bc8ab1aa17dfb9b5b8539773862cba5
David R. Myers <davidrmyersii@gmail.com>
2024-10-28 00:51:53 -0400

    Add `jsx-lite`

---
+++ lib/jsx-lite/index.ts
@@ -0,0 +1,53 @@
+import van from 'vanjs-core'
+
+export * from './types'
+
+const adaptProps = (rawProps: Record<string, any>): Record<string, any> => {
+  const adapted: Record<string, any> = {}
+
+  for (const key in rawProps) {
+    adapted[key.toLowerCase()] = rawProps[key]
+  }
+
+  return adapted
+}
+
+export const createElement = (tag: string | Function, { children, ...rawProps }: {
+  children?: Element | undefined,
+}): Element => {
+  const props = adaptProps(rawProps)
+
+  if (typeof tag === 'function') {
+    return tag({ children, ...props })
+  }
+
+  if (typeof tag === 'string') {
+    if (typeof children === 'undefined') {
+      return van.tags[tag](props)
+    }
+
+    if (Array.isArray(children)) {
+      return van.tags[tag](props, ...children)
+    }
+
+    return van.tags[tag](props, children)
+  }
+
+  throw new TypeError('Invalid tag')
+}
+
+export {
+  createElement as jsx,
+  createElement as jsxDEV,
+  createElement as jsxs,
+}
+
+export const renderToString = (element: JSX.Element): string => {
+  if (import.meta.env.SSR) {
+    if (typeof element === 'object' && 'render' in element && typeof element.render === 'function') {
+      return element.render()
+    }
+  }
+
+  return ''
+}
---
+++ lib/jsx-lite/jsx.d.ts
@@ -0,0 +1,23 @@
+import { JSXElementType, VanElement, InnerElement, Key, TagOption } from './types'
+
+declare global {
+  declare namespace JSX {
+    type ElementType = string | JSXElementType<any>
+    interface ElementAttributesProperty {
+        props: object,
+    }
+    interface ElementChildrenAttribute {
+        children: object,
+    }
+    interface Element extends VanElement {
+    }
+    interface IntrinsicAttributes {
+        key?: Key,
+    }
+    type IntrinsicElements = {
+        [K in keyof InnerElement]: TagOption<K>;
+    }
+  }
+}
+
+export {}
