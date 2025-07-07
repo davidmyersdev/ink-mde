baff55c413a841d82c70dee1d344b83b2c60fc33
David R. Myers <davidrmyersii@gmail.com>
2025-07-07 17:31:29 -0400

    Update all dependencies

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -4,7 +4,20 @@ import type InkInternal from '/types/internal'
 import { blankState } from '../store'
 import { Root } from './components/root'
 
-const AppContext = createContext<InkInternal.Store>([() => blankState(), value => (typeof value === 'function' ? value(blankState()) : value)])
+const AppContext = createContext<InkInternal.Store>([
+  () => blankState(),
+  (value) => {
+    if (typeof value === 'function') {
+      const state = blankState()
+
+      // @ts-expect-error https://github.com/microsoft/TypeScript/issues/37663
+      return value(state)
+    }
+
+    return value
+  },
+])
+
 const AppProvider: Component<{ children: JSX.Element, store: InkInternal.Store }> = (props) => {
   return (
     // eslint-disable-next-line solid/reactivity
