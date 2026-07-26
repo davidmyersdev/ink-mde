a99cb19cadcd61427a49ad3cd7166347edd74bf2
David Myers <davidrmyersii@gmail.com>
2026-07-26 01:04:01 -0400

    Complete autocomplete specs

--- test/e2e/plugins/autocomplete.spec.ts
+++ test/e2e/plugins/autocomplete.spec.ts
@@ -60,6 +60,28 @@ test.describe('autocomplete', () => {
     await expect(content).toHaveText('[http://example.test/hello]')
   })
 
+  test('updates bracket completion after reconfiguration', async ({ page }) => {
+    const { content, host } = getLocators(page)
+
+    await withInk(page, async ({ mount, target }) => {
+      const instance = await mount(target, { interface: { autocomplete: true } })
+      Object.assign(target, { instance })
+      instance.focus()
+    })
+    await page.keyboard.type('[')
+    await expect(content).toHaveText('[]')
+    await host.evaluate(async (target: HTMLElement & {
+      instance: { reconfigure: (options: { interface: { autocomplete: boolean } }) => Promise<void>, update: (doc: string) => void },
+    }) => {
+      await target.instance.reconfigure({ interface: { autocomplete: false } })
+      target.instance.update('')
+    })
+    await content.click()
+    await page.keyboard.type('[')
+
+    await expect(content).toHaveText('[')
+  })
+
   test.describe('when disabled', () => {
     test('does not auto-close square brackets', async ({ page }) => {
       const { content } = getLocators(page)
